96bc7d1f483ed67f4c73b6273d473662c8d83688
wahjay <hotan38@berkeley.edu>
2021-02-10 20:06:53 -0800

    fixed typo

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   return res.send("up and running");
